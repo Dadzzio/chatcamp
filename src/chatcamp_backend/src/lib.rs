6a7ff667d78f3ae36eddbf14126ed42c8f787f43
Dadzzio <szymon09payerhin@gmail.com>
2024-07-30 20:00:31 +0200

    dzien2

--- src/chatcamp_backend/src/lib.rs
+++ src/chatcamp_backend/src/lib.rs
@@ -1,18 +1,37 @@
-use std::{cell::{Ref, RefCell}, collections::HashMap};
+use std::{cell::RefCell, collections::HashMap};
 
+use candid::Principal;
+use ic_cdk::caller;
 
 thread_local! {
-    static NOTES: RefCell<Vec<String>> = RefCell::default();
+    static CHAT: RefCell<HashMap<[Principal; 2], Vec<String>>> = RefCell::default();
+
 }
 
+
 #[ic_cdk::query]
-fn get_notes() -> Vec<String> {
-    NOTES.with_borrow(|notes| notes.clone())
+fn get_chat(user_path: [Principal; 2]) -> Option<Vec<String>> {
+    CHAT.with_borrow(|chats|  chats.get(&user_path).cloned())
 }
 
 #[ic_cdk::update]
-fn add_note(note: String) {
-    NOTES.with_borrow_mut(|notes| {
-        notes.push(note)
+fn add_chat_msg(msg: String, user2: Principal) {
+    let user1 = caller();
+
+    if user1 == Principal::anonymous() {
+        panic!("Anonymous Principal!")
+    }
+
+    let mut principals = [user1, user2];
+    principals.sort();
+
+    CHAT.with_borrow_mut(|chats| {
+        let mut_chat = chats.get_mut(&principals);
+
+        if let Some(chat_vec) = mut_chat {
+            chat_vec.push(msg);
+        } else {
+            chats.insert(principals, vec![msg]);
+        }
     })
 }
--- src/chatcamp_frontend/src/App.vue
+++ src/chatcamp_frontend/src/App.vue
@@ -1,25 +1,85 @@
 <script lang="ts">
 import { ref } from 'vue';
-import { chatcamp_backend } from '../../declarations/chatcamp_backend';
+import { canisterId, chatcamp_backend, createActor } from '../../declarations/chatcamp_backend';
+import { AuthClient } from '@dfinity/auth-client';
+import { HttpAgent } from '@dfinity/agent';
+import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
 
 export default {
   data() {
     return {
-      newNote: "",
-      notes: [] as string[]
+      newchat: "",
+      chats: [] as string[][],
+      identity: undefined as undefined | Identity,
+      principal : undefined as undefined | Principal,
+      targetPrincipal: "",
     }
   },
   methods: {
-    async dodajNotatke() {
-      await chatcamp_backend.add_note(this.newNote)
-      await this.pobierzNotatki()
+    isUserLogged() {
+      if (!this.identity || !this.principal || this.principal === Principal.anonymous()){
+        throw new Error("User is not logged in.")
+      }
+      return {
+        identity: this.identity,
+        principal : this.principal
+      }
     },
-    async pobierzNotatki() {
-      this.notes = await chatcamp_backend.get_notes()
+    validateTargetPrincipal() {
+      const cleanTargetPrincipal = this.targetPrincipal.trim()
+      if (this.targetPrincipal === ""){
+        throw new Error("Wrong target.")
+      }
+      const targetPrincipal = Principal.fromText(cleanTargetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
+        throw new Error("Wrong target.")
+      }
+      return targetPrincipal
+
+    },
+    getAuthClient(){
+      this.isUserLogged()
+      return createActor(canisterId, {
+        agentOptions : {
+          identity: this.identity
+        }
+      })
+    },
+    async dodajChatMSG() { 
+      this.isUserLogged()
+      const targetPrincipal = this.validateTargetPrincipal()
+      const backend = this.getAuthClient()
+      await backend.add_chat_msg(this.newchat, targetPrincipal)
+      await this.pobierzCzaty()
+    },
+    async pobierzCzaty() {
+      const {identity, principal} = this.isUserLogged()
+      const targetPrincipal = this.validateTargetPrincipal()
+      const chatPath = [identity.getPrincipal(), targetPrincipal].sort()
+      this.chats = await chatcamp_backend.get_chat(chatPath)
+    },
+    
+    
+    async login(){
+      const authClient = await AuthClient.create();
+      await authClient.login(
+        {identityProvider: 'http://b77ix-eeaaa-aaaaa-qaada-cai.localhost:4943/',
+          onSuccess: async () => {
+            const identity = authClient.getIdentity();
+            this.principal = identity.getPrincipal()
+            console.log("Logged IN!", this.principal)
+            this.identity = identity;
+            await this.pobierzCzaty()
+          }
+        }
+      )
+      
+      
+      
+
+      
     }
-  },
-  mounted(){
-    this.pobierzNotatki()
   }
 }
 </script>
@@ -29,11 +89,17 @@ export default {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-      <div>
-      {{ notes }}
+    {{ principal }} <button @click="login">login</button>
+    <div>
+      <input v-model="targetPrincipal"><button @click="pobierzCzaty">Pobierz Chat</button>
     </div>
     <div>
-      <textarea v-model="newNote"></textarea><button @click="dodajNotatke">Dodaj notatke</button>
+      <div v-for="chat in chats[0]">
+        {{ chat }}
+      </div>
+    </div>
+    <div>
+      <textarea v-model="newchat"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
     </div>
   </main>
 </template>
