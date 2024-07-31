dda405b3ceacbefe662f986f3bed39fa8567ea74
Dadzzio <szymon09payerhin@gmail.com>
2024-07-31 19:17:53 +0200

    dzien3

--- src/chatcamp_backend/src/lib.rs
+++ src/chatcamp_backend/src/lib.rs
@@ -2,16 +2,37 @@ use std::{cell::RefCell, collections::HashMap};
 
 use candid::Principal;
 use ic_cdk::caller;
+use user::UserData;
+pub mod user;
 
 thread_local! {
     static CHAT: RefCell<HashMap<[Principal; 2], Vec<String>>> = RefCell::default();
-
+    static USERS: RefCell<HashMap<Principal, UserData>> = RefCell::default()
 }
 
+#[ic_cdk::update]
+fn register_user(nick: String) {
+    let user: Principal = caller();
+
+    if user == Principal::anonymous() {
+        panic!("Anonymous user!")
+    }
+    USERS.with_borrow_mut(|users: &mut HashMap<Principal, UserData>| {
+        users.insert(user, UserData::new(nick))
+    });
+}
+#[ic_cdk::query]
+fn get_users() -> HashMap<Principal, UserData> {
+    USERS.with_borrow(|users| users.clone())
+}
+#[ic_cdk::query]
+fn get_user(user: Principal) -> Option<UserData> {
+    USERS.with_borrow(|users| users.get(&user).cloned())
+}
 
 #[ic_cdk::query]
 fn get_chat(user_path: [Principal; 2]) -> Option<Vec<String>> {
-    CHAT.with_borrow(|chats|  chats.get(&user_path).cloned())
+    CHAT.with_borrow(|chats| chats.get(&user_path).cloned())
 }
 
 #[ic_cdk::update]
@@ -21,7 +42,10 @@ fn add_chat_msg(msg: String, user2: Principal) {
     if user1 == Principal::anonymous() {
         panic!("Anonymous Principal!")
     }
-
+    let is_user_registered = USERS.with_borrow(|users| users.contains_key(&user1));
+    if !is_user_registered {
+        panic!("User in not registered!");
+    }
     let mut principals = [user1, user2];
     principals.sort();
 
--- src/chatcamp_frontend/src/App.vue
+++ src/chatcamp_frontend/src/App.vue
@@ -1,10 +1,11 @@
 <script lang="ts">
-import { ref } from 'vue';
+import { ref, registerRuntimeCompiler } from 'vue';
 import { canisterId, chatcamp_backend, createActor } from '../../declarations/chatcamp_backend';
 import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import type { Identity } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
+import type { UserData } from '../../declarations/chatcamp_backend/chatcamp_backend.did';
 
 export default {
   data() {
@@ -14,6 +15,9 @@ export default {
       identity: undefined as undefined | Identity,
       principal : undefined as undefined | Principal,
       targetPrincipal: "",
+      userData: undefined as undefined | UserData,
+      newNickname: "",
+      allUsers: [] as [Principal, UserData][],
     }
   },
   methods: {
@@ -46,6 +50,16 @@ export default {
         }
       })
     },
+    async getUserData(){
+      const {identity, principal} = this.isUserLogged()
+      const maybeUserData = await chatcamp_backend.get_user(principal as Principal)
+      if (maybeUserData.length === 0){
+        this.userData = undefined
+      } else {
+        this.userData = maybeUserData[0]
+      }
+      console.log("UserData: ", this.userData)
+    },
     async dodajChatMSG() { 
       this.isUserLogged()
       const targetPrincipal = this.validateTargetPrincipal()
@@ -67,18 +81,35 @@ export default {
         {identityProvider: 'http://b77ix-eeaaa-aaaaa-qaada-cai.localhost:4943/',
           onSuccess: async () => {
             const identity = authClient.getIdentity();
-            this.principal = identity.getPrincipal()
+            this.principal = identity.getPrincipal();
+            const principal =  identity.getPrincipal();
             console.log("Logged IN!", this.principal)
             this.identity = identity;
-            await this.pobierzCzaty()
+        
+            console.log("UserData: ", this.userData)
+            await this.getUserData()
+            await this.getAllUsers()
           }
         }
       )
-      
-      
-      
-
-      
+    },
+    async logout() {
+      const authClient = await AuthClient.create();
+      await authClient.logout()
+      this.identity = undefined;
+      this.principal = undefined;
+      this.chats = [];
+      this.userData = undefined; 
+    },
+    async registerNickname() {
+      const trimedNickname = this.newNickname.trim()
+      const backend = this.getAuthClient()
+      await backend.register_user(trimedNickname)
+      await this.getUserData()
+      await this.getAllUsers()
+    },
+    async getAllUsers(){
+      this.allUsers =  await chatcamp_backend.get_users()
     }
   }
 }
@@ -89,9 +120,19 @@ export default {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    {{ principal }} <button @click="login">login</button>
-    <div>
-      <input v-model="targetPrincipal"><button @click="pobierzCzaty">Pobierz Chat</button>
+    <button v-if="!principal" @click="login">login</button>
+    <button v-if="principal" @click="logout">logout</button>
+    <div v-if="principal && !userData">
+      <input v-model="newNickname" placeholder="wpisz nick"> <button @click="registerNickname">Zarejestruj się</button>
+    </div>
+    <div v-if="principal && userData">
+      <h1>Witaj, {{ userData.nickname }}</h1>
+    <div v-if="allUsers">
+      <select v-model="targetPrincipal" @change="pobierzCzaty">
+        <option disabled value="">Please select one</option>
+        <option v-for="[userPrincipal, userData] in allUsers" :value="userPrincipal.toText()">{{ userData.nickname }}</option>
+      </select>
+\
     </div>
     <div>
       <div v-for="chat in chats[0]">
@@ -99,7 +140,7 @@ export default {
       </div>
     </div>
     <div>
-      <textarea v-model="newchat"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
-    </div>
+      <textarea v-model="newchat" placeholder="Wiadomość"></textarea><button @click="dodajChatMSG">Wyślij wiadomosć</button>
+    </div></div>
   </main>
 </template>
